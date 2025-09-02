390846ca003e570f9572b327131cf150ad021f31
Fábio Veiga <veigabfabio@hotmail.com>
2025-09-02 11:48:42 -0300

    Ajutes e novo script em package

--- frontend/dist/app.js
+++
@@ -1,2 +0,0 @@
-import CalendarioController from "./controllers/calendarioController.js";
-const calendarioController = new CalendarioController();
--- frontend/dist/controllers/calendarioController.js
+++
@@ -1,9 +0,0 @@
-import CalendarioView from "../views/calendarioView.js";
-class CalendarioController {
-    constructor() {
-        this.calendarioView = new CalendarioView();
-        console.log("CalendarioController Criado");
-        this.calendarioView.criarElementos();
-    }
-}
-export default CalendarioController;
