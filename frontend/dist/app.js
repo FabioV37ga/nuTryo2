c4f1a162c4708a3d3fd850cb843dfdc54e8fb10c
Fábio Veiga <veigabfabio@hotmail.com>
2025-09-02 17:38:08 -0300

    Removendo commit do dist/ não intencional, e alterando gitignore

--- frontend/dist/app.js
+++
@@ -1,4 +0,0 @@
-import CalendarioController from "./controllers/calendarioController.js";
-import JanelaController from "./controllers/janelaController.js";
-const calendarioController = new CalendarioController();
-const janelaController = new JanelaController();
