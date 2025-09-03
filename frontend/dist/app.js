1172ec7cb24a21bbddab75658b124b0fc339a9ec
Fábio Veiga <veigabfabio@hotmail.com>
2025-09-03 16:53:39 -0300

    correção no gitignore

--- frontend/dist/app.js
+++
@@ -1,6 +0,0 @@
-import CalendarioController from "./controllers/calendarioController.js";
-import DiaController from "./controllers/diaController.js";
-import JanelaController from "./controllers/janelaController.js";
-const calendarioController = new CalendarioController();
-const janelaController = new JanelaController();
-const diaController = new DiaController();
