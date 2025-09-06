fd706e75585b5f4f695fc03ff52e9db5ca19903c
Fábio Veiga <veigabfabio@hotmail.com>
2025-09-06 04:39:11 -0300

    ?

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename);
 
 const PORT = 3001;
 
-app.use(express.static(join(__dirname, 'public')));
+// app.use(express.static(join(__dirname, 'public')));
 
 app.listen(PORT, () =>{
     console.log('Servidor rodando na porta 3001');
