138e94ea47466bb1a274c6c9e46a0f3694fb0dde
Fábio Veiga <veigabfabio@hotmail.com>
2025-09-13 20:14:39 -0300

    Comentários código do controller de refeicao

--- frontend/src/controllers/refeicaoController.ts
+++ frontend/src/controllers/refeicaoController.ts
@@ -8,16 +8,22 @@ class RefeicaoController {
     listaTipoLabel: Element;
     listaTipos: Element;
     listaTiposItem: NodeListOf<Element>;
+
     constructor() {
+        // Atribui aos atributos da classe os elementos de refeição
         this.listaTipoLabel = document.querySelector(".refeicao-tipo-tipoSelecionado") as Element;
         this.listaTipos = document.querySelector(".refeicao-tipo-list") as Element;
         this.listaTiposItem = document.querySelectorAll(".refeicao-tipo-list li") as NodeListOf<Element>;
+
+        // Chama função para adicionar eventos de click
         this.adicionaEventosDeClick()
     }
 
     private adicionaEventosDeClick() {
 
-        // Abre a lista ao clicar no tipo selecionado
+        // ------------------------------------------------------------------------------------------------------------------------------------------------------
+        //  # Abre a lista ao clicar no tipo selecionado
+        
         // Previne adição multipla de eventos de click
         if (!this.listaTipoLabel.classList.contains("hasEvent")) {
             this.listaTipoLabel.classList.add("hasEvent")
@@ -30,16 +36,26 @@ class RefeicaoController {
             })
         }
 
-        // Seleciona o tipo, e fecha a lista de tipos
+        // ------------------------------------------------------------------------------------------------------------------------------------------------------
+        // # Adiciona eventos para seleção do tipo da refeição
+
+        // Loop para adicionar eventos em todos elementos
         for (let i = 0; i <= this.listaTiposItem.length - 1; i++) {
+
             // Previne adição multipla de eventos de click
             if (!this.listaTiposItem[i].classList.contains("hasEvent")) {
                 this.listaTiposItem[i].classList.add("hasEvent")
 
+                // adiciona evento de click
                 this.listaTiposItem[i].addEventListener("click", (e) => {
                     e.stopPropagation
+
+                    // Armazena target em elemento clicado
                     var elementoClicado = e.currentTarget as HTMLElement
+
+                    // Seleciona visualmente o tipo da refeição
                     this.refeicaoView.selecionaItem(elementoClicado)
+                    // esconde os tipos da refeição
                     this.refeicaoView.toggleListaDeTipos()
 
                     // Ao clicar no tipo da refeição, retorna refeição do banco de dados
@@ -48,15 +64,13 @@ class RefeicaoController {
                         elementoClicado.parentElement?.parentElement?.getAttribute("value") as string
                     )
 
-                    // Se houver retorno do banco, apenas troca o tipo da refeição.
+                    // Se houver retorno do banco, apenas troca o tipo da refeição da refeição existente
                     if (refeicao){
                         diaObjeto.editarTipoRefeicao(refeicao._id as string, elementoClicado.textContent)
-                        // refeicao.tipo = elementoClicado.textContent
-                        // Aqui vai precisar salvar
                     }
                     // Se não houver retorno do banco, significa que refeição não existe, e precisa ser criada.
                     else{
-                        
+                        // Gera um objeto de refeição local sem alimentos
                         diaObjeto.gerarRefeicao(
                            Number(elementoClicado.parentElement?.parentElement?.getAttribute("value") as string), 
                             elementoClicado.textContent,
@@ -64,11 +78,11 @@ class RefeicaoController {
                         )
                     }
 
+                    // Pega o id da refeição sendo manipulada
                     var refeicaoAtual = document.querySelector(".abaSelecionada")?.getAttribute("value") as string
+                    
+                    // Troca visualmente o tipo da refeição sendo manipulada
                     this.refeicaoView.trocaTipo(refeicaoAtual, elementoClicado.textContent)
-                    // console.log("#RefeicaoController - Nova refeição criada")
-                    // console.log(diaObjeto.diasSalvos)
-                    // console.log(elementoClicado.textContent)
                 })
 
             }
