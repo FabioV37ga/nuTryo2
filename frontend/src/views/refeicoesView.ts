57608291db43a6ca432c756dd790b2e3aa81c671
Fábio Veiga <veigabfabio@hotmail.com>
2025-09-14 00:49:14 -0300

    Comentários na view de refeições

--- frontend/src/views/refeicoesView.ts
+++ frontend/src/views/refeicoesView.ts
@@ -9,18 +9,27 @@ class RefeicoesView extends JanelaView {
         super()
     }
 
+    // ------------------------------------------------------------------------------------------------------------------------------------------------------
+    // # Método responsável por adicionar uma refeição a janela
     adicionarRefeicao(refeicao?: any) {
+        // Atribui ID
         this.id = RefeicoesView._id
+        // Incrementa ID + 1 para adiciona a proxima refeição
         RefeicoesView._id += 1
 
+        // Inicializa Strings de tipo e label da refeição
         var tipo: string = "";
         var stringAlimentos: string = "";
 
+        // Se houver atributo de refeição...
         if (refeicao) {
+            // Define o tipo, se houver
             tipo = refeicao.tipo ? refeicao.tipo : ""
             var alimentos = refeicao.alimentos
 
+            // Se houverem alimentos na refeição...
             if (alimentos) {
+                // Adiciona título aos alimentos criados (Alimentos que compõem a refeição)
                 for (let i = 0; i <= alimentos.length - 1; i++) {
                     if (i < alimentos.length - 1) {
                         stringAlimentos += `${alimentos[i].alimento} • `
@@ -31,7 +40,7 @@ class RefeicoesView extends JanelaView {
             }
         }
 
-
+        // Modelo do elemento a ser criado no DOM
         const elemento =
             `<div class="refeicao" value="${this.id}">
             <a class="botao-editar-refeicao">
@@ -46,21 +55,28 @@ class RefeicoesView extends JanelaView {
             </div>
         </div>`
 
+        // cria elemento
         $(".lista-de-refeicoes").append(elemento)
     }
 
+    // ------------------------------------------------------------------------------------------------------------------------------------------------------
+    // # Método responsável por remover visualmente uma refeição
     removerRefeicao(refeicao: Element) {
+        // Armazena ID da refeição a ser apagada (passado dentro do elemento passado como parâmetro do método)
         var idApagado: number = parseInt(refeicao.getAttribute("value") as string)
+
+        // Elementos de refeição restantes no DOM
         var elementosRestantes = document.querySelectorAll(".refeicao.hasEvent")
+
+        // Se houverem elementos restantes...
         if (elementosRestantes) {
+            // Decrementa o ID das refeições restantes (Se houverem ID maior do que o ID apagado)
             for (let i = idApagado; i <= elementosRestantes.length - 1; i++) {
                 elementosRestantes[i].setAttribute("value", String(parseInt(elementosRestantes[i].getAttribute("value") as string) - 1))
             }
         }
+        // Apaga refeição passada como parâmetro
         refeicao.remove()
-    }
-    log() {
-        console.log("Aves!")
     }
 }
 
